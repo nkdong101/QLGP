9a1b680906d881521354de8d5a4b6da0b8b0fdee
nkdong101 <110988513+nkdong101@users.noreply.github.com>
2024-08-07 17:31:32 +0700

    ok

--- components/chart/nodeSvg.vue
+++ components/chart/nodeSvg.vue
@@ -11,7 +11,7 @@
       :width="config.Width"
       fill="#ffffff"
       stroke-width="3"
-      :stroke="getFillColor(data.gender)"
+      :stroke="getFillColor(data.gioitinh)"
       rx="5"
       ry="5"
     ></rect>
@@ -20,8 +20,8 @@
       y="0"
       height="20"
       :width="config.Width"
-      :fill="getFillColor(data.gender)"
-      stroke-width="1"
+      :fill="getFillColor(data.gioitinh)"
+      stroke-width="0"
       stroke="#b1b9be"
       rx="5"
       ry="5"
@@ -32,42 +32,70 @@
       :x2="config.Width"
       y2="20"
       stroke-width="5"
-      :stroke="getFillColor(data.gender)"
+      :stroke="getFillColor(data.gioitinh)"
     ></line>
+    <foreignObject
+      cursor="pointer"
+      @click="$emit('nameClick', data)"
+      :style="{
+        fontSize: '14px',
+      }"
+      x="100"
+      y="45"
+      width="140"
+      height="30"
+      ><div class="text-container" xmlns="http://www.w3.org/1999/xhtml">
+        {{ data.name }}
+       
+      </div>
+    </foreignObject>
+
     <text
       data-width="160"
       data-text-overflow="multiline"
-      style="font-size: 14px"
+      style="font-size: 12px"
       fill="black"
       x="100"
-      y="66"
+      y="95"
       text-anchor="start"
-      ><tspan x="100" y="66" text-anchor="start">{{ data.name }}</tspan></text
+      ><tspan x="100" y="90" text-anchor="start"
+        >{{ data.Namsinh }}
+        {{ data.Nammat ? " - " + data.Nammat : "" }}
+      </tspan></text
     >
     <use xlink:href="#base_img_0_stroke" />
     <circle id="base_img_0_stroke" fill="#b1b9be" cx="45" cy="62" r="37" />
     <image
       preserveAspectRatio="xMidYMid slice"
       clip-path="url(#base_img_0)"
-      xlink:href="https://www.industrialempathy.com/img/remote/ZiClJf-640w.avif"
-      x="10"
-      y="26"
+      :xlink:href="
+        data.Avatar
+          ? '/Images/avatar/' + data.Avatar.split('|')[0]
+          : '/Images/avatar/user.png'
+      "
+      :x="10"
+      :y="data.Avatar ? '26' : '26'"
       width="72"
-      height="72"
+      height="78"
     ></image>
+
     <!-- <el-popover
-    
-    placement="top-start"
-    title="Title"
-    width="200"
-    trigger="click"
-    content="this is content, this is content, this is content">
-    <el-button :ref="'pop' + data.id" slot="reference">Hover to activate</el-button>
-    </el-popover> -->
-    <use @click="handClickdot" :data-ctrl-n-menu-id="data.id" x="210" y="95" xlink:href="#base_node_menu"></use>
-    
-      
+      :ref="'pop' + data.id"
+      placement="top-start"
+      title="Title"
+      width="200"
+      trigger="click"
+      content="This is content, this is content, this is content"
+    >
      
+    </el-popover> -->
+    <use
+      @click="handClickdot"
+      :data-ctrl-n-menu-id="data.id"
+      x="210"
+      y="95"
+      xlink:href="#base_node_menu"
+    ></use>
   </g>
 </template>
 <script>
@@ -81,16 +109,17 @@ export default {
     },
   },
   methods: {
-    handClickdot(){
-      console.log("handClickdotdotdotdot")
+    handClickdot() {
+      // console.log("handClickdotdotdotdot");
       // console.log(this.$refs['pop'+this.data.id])
-      // this.$refs['pop'+this.data.id].click();
+      // this.$refs["pop" + this.data.id].doToggle();
+      // this.$emit('dotClick',this);
     },
-    getFillColor(gender) {
-      return gender === "male" ? "#6bb4df" : "#cb4aaf";
+    getFillColor(gioitinh) {
+      return gioitinh === 1 ? "#6bb4df" : "#cb4aaf";
     },
     handleClick() {
-      console.log(this.data);
+      // console.log(this.data);
     },
   },
 
@@ -99,4 +128,12 @@ export default {
   },
 };
 </script>
-<style lang=""></style>
+<style lang="scss">
+.text-container {
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+  vertical-align: sub;
+  word-break: break-word;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div style="height: 100%; overflow: auto">
+  <div
+    style="height: 100%; overflow: hidden"
+    @wheel="onWheel"
+    :style="{
+      cursor: isPanning ? 'move' : 'auto',
+    }"
+  >
     <!-- <InputContainer style="width: 200px" label="Dòng họ" :labelWidth="100">
       <InputSelect
         @input="ChangeInpt"
@@ -14,32 +20,84 @@
       :nodes="nodes"
       :model="model"
     /> -->
-    <div ref="svgContainer" :style="svgStyle">
-      <svg :viewBox="viewBox" style="height: 100%; width: 100%; display: block">
+    <div
+      ref="svgContainer"
+      class="svgContainer"
+      style="
+        height: 100%;
+        width: 100%;
+        overflow: auto;
+        display: block;
+        position: relative;
+      "
+    >
+      <svg
+        @mousedown="startPan"
+        @mousemove="pan"
+        @mouseup="endPan"
+        @mouseleave="endPan"
+        ref="svgElement"
+        :viewBox="viewBox"
+        style="height: 100%; width: 100%; overflow-x: auto; overflow-y: auto"
+      >
         <defs>
-          <g id="base_node_menu" style="cursor:pointer;"><rect x="0" y="0" fill="transparent" width="22" height="22"></rect><circle cx="4" cy="11" r="2" fill="#aeaeae"></circle><circle cx="11" cy="11" r="2" fill="#aeaeae"></circle><circle cx="18" cy="11" r="2" fill="#aeaeae"></circle></g>
+          <g id="base_node_menu" style="cursor: pointer">
+            <rect x="0" y="0" fill="transparent" width="22" height="22"></rect>
+            <circle cx="4" cy="11" r="2" fill="#aeaeae"></circle>
+            <circle cx="11" cy="11" r="2" fill="#aeaeae"></circle>
+            <circle cx="18" cy="11" r="2" fill="#aeaeae"></circle>
+          </g>
+
+          <!-- <symbol id="base_node_menu" viewBox="0 0 24 24">
+            <rect x="0" y="0" fill="transparent" width="22" height="22"></rect>
+            <circle cx="4" cy="11" r="2" fill="#aeaeae"></circle>
+            <circle cx="11" cy="11" r="2" fill="#aeaeae"></circle>
+            <circle cx="18" cy="11" r="2" fill="#aeaeae"></circle>
+  </symbol> -->
           <clipPath id="base_img_0">
             <circle id="base_img_0_stroke" cx="45" cy="62" r="35" />
           </clipPath>
         </defs>
-
-        <g data-l-id="[22][23]" class="link right-partner">
+        <g
+          :data-l-id="`[${item.Id1}][${item.Id2}]`"
+          v-for="item in nodes.Lines"
+          :key="item.Points"
+        >
           <path
             stroke-linejoin="round"
             stroke="#aeaeae"
             stroke-width="1px"
             fill="none"
-            d="M326.25,60 L366.25,60"
+            :d="item.Points"
           ></path>
         </g>
         <nodeSvg
           v-for="(item, index) in nodes.Data"
           :key="item.id"
           :data="item"
+          :ref="'node' + item.id"
           :config="nodes.Config"
           :transform="`translate(${item.Box.X}, ${item.Box.Y})`"
+
+          @nameClick="nameClick"
         />
       </svg>
+
+      <el-popover
+        :style="nodePoperStyles"
+        class="btn-p"
+        placement="right"
+        popper-class="btn-poper"
+        ref="pop"
+        width="100"
+        trigger="manual"     
+      >
+
+      <div >
+        <el-button>aaa</el-button>
+      </div>
+        <el-button style="opacity: 1;padding:0" slot="reference"></el-button>
+      </el-popover>
     </div>
 
     <DefaultForm :model="form" @actionOK="form.Save.call(this)">
@@ -68,18 +126,25 @@ export default {
       return {
         height: this.nodes.Config.MaxHeight + "px",
         // width: this.nodes.Config.MaxWidth + "px",
-        maxWidth: this.nodes.Config.MaxWidth + "px",
+        width: this.nodes.Config.MaxWidth + "px",
       };
     },
   },
   data() {
     return {
-      viewBox: "0 0 600 500",
+      lastClickPosition: { x: 0, y: 0 },
+      nodeClicked: null,
+      isPanning: null,
+      viewBox: "561 -7 648 729",
       viewBoxImg: "0 200 200 200",
       dongho_id: null,
       model: {
         enableSearch: false,
         template: "myTemplate",
+      },
+      nodePoperStyles: {
+        top: `0px`,
+        left: `0px`,
       },
       nodes: {
         Config: {},
@@ -117,13 +182,46 @@ export default {
           });
         },
         Save: () => {
-          //   this.Save();
           console.log("save", this.family);
         },
       }),
     };
   },
   methods: {
+   
+    nameClick(obj){
+      this.form.ShowForm('Thông tin',obj,true);
+    },
+
+    startPan(event) {
+      if (event.button !== 0) return; // Only respond to left mouse button
+      this.isPanning = true;
+      this.startPoint = { x: event.clientX, y: event.clientY };
+      const viewBoxValues = this.viewBox.split(" ").map(Number);
+      this.startViewBox = {
+        x: viewBoxValues[0],
+        y: viewBoxValues[1],
+        width: viewBoxValues[2],
+        height: viewBoxValues[3],
+      };
+    },
+    pan(event) {
+      if (!this.isPanning) return;
+
+      const dx =
+        (this.startPoint.x - event.clientX) *
+        (this.startViewBox.width / this.$refs.svgElement.clientWidth);
+      const dy =
+        (this.startPoint.y - event.clientY) *
+        (this.startViewBox.height / this.$refs.svgElement.clientHeight);
+
+      this.viewBox = `${this.startViewBox.x + dx} ${this.startViewBox.y + dy} ${
+        this.startViewBox.width
+      } ${this.startViewBox.height}`;
+    },
+    endPan() {
+      this.isPanning = false;
+    },
     nodePosition(item, index) {
       console.log(item);
       if (item.fid && item.mid) {
@@ -179,21 +277,88 @@ export default {
     },
     onWheel(e) {
       // console.log(e);
+      // this.$refs.pop.doDestroy();
       if (e.ctrlKey) {
-        e.preventDefault();
-        const scaleAmount = e.deltaY > 0 ? 1.1 : 0.9;
-        const [x, y, width, height] = this.viewBox.split(" ").map(Number);
-        const [xImg, yImg, widthImg, heightImg] = this.viewBoxImg
-          .split(" ")
-          .map(Number);
-        const newWidth = width * scaleAmount;
-        const newHeight = height * scaleAmount;
-        const nwidthImg = widthImg * scaleAmount;
-        const nheightImg = heightImg * scaleAmount;
-        this.viewBox = `${x} ${y} ${newWidth} ${newHeight}`;
-        this.viewBoxImg = `${xImg} ${yImg} ${nwidthImg} ${nheightImg}`;
+        
+        e.preventDefault(); 
+        const svgElement = this.$refs.svgElement;
+       
+        const { left, top, width, height } = svgElement.getBoundingClientRect();
+        const mouseX = e.clientX - left;
+        const mouseY = e.clientY - top;
+        const zoomFactor = e.deltaY > 0 ? 1.1 : 0.9;
+
+        const viewBoxValues = this.viewBox.split(" ").map(Number);
+        const [x, y, viewBoxWidth, viewBoxHeight] = viewBoxValues;
+
+        const newWidth = viewBoxWidth * zoomFactor;
+        const newHeight = viewBoxHeight * zoomFactor;
+
+        const dx = (mouseX / width) * (viewBoxWidth - newWidth);
+        const dy = (mouseY / height) * (viewBoxHeight - newHeight);
+
+        this.viewBox = `${x + dx} ${y + dy} ${newWidth} ${newHeight}`;
+        // if (this.nodeClicked) {
+        //   const node = this.nodeClicked.$el;
+
+        //   if (node) {
+        //     const rect = node.getBoundingClientRect();
+
+        //     this.nodePoperStyles = `transform: translate(${rect.x}px,${rect.y}px)`;
+        //     // this.showPopover(rect.top + window.scrollY, rect.left + window.scrollX);
+        //   }
+        // this.$refs.pop.doDestroy();
+        this.updatePopoverPosition();
       }
     },
+    updatePopoverPosition() {
+      const { x, y } = this.lastClickPosition;
+      // Convert coordinates to viewBox-relative coordinates
+      const [viewBoxX, viewBoxY, viewBoxWidth, viewBoxHeight] = this.viewBox
+        .split(" ")
+        .map(Number);
+      const svgWidth = this.$refs.svgElement.getBoundingClientRect().width;
+      const svgHeight = this.$refs.svgElement.getBoundingClientRect().height;
+
+      const scaleX = svgWidth / viewBoxWidth;
+      const scaleY = svgHeight / viewBoxHeight;
+
+      const relativeX = (x - viewBoxX) * scaleX;
+      const relativeY = (y - viewBoxY) * scaleY;
+
+      this.nodePoperStyles = {
+        position: "absolute",
+        top: `${relativeY}px`,
+        left: `${relativeX}px`,
+        transform: "none", // Ensure no additional transform interferes
+      };
+    },
+
+    // dotClick(item) {
+    //   this.nodeClicked = item;
+    //   if(this.$refs.pop.showPopper){
+    //     this.$refs.pop.doClose();
+    //   }
+    //   this.$nextTick(() => {
+    //     const node = item.$el; // Adjusted for Vue v-for array ref
+    //     if (node) {
+    //       const rect = node.getBoundingClientRect();
+    //       this.lastClickPosition = {
+    //         x: rect.left + window.scrollX,
+    //         y: rect.top + window.scrollY,
+    //       };
+    //       // this.updatePopoverPosition();
+    //       this.nodePoperStyles = {
+
+    //         top: `${rect.y}px`,
+    //         left: `${rect.x}px`,
+    //         transform: "none", // Ensure no additional transform interferes
+    //       };
+    //       console.log(this.$refs.pop)
+    //       this.$refs.pop.doToggle();
+    //     }
+    //   });
+    // },
     nodeClick() {
       console.log("click");
     },
@@ -249,5 +414,11 @@ export default {
 }
 .fm /deep/ .form-info .form-info-c .el-row {
   justify-content: center;
+}
+
+.svgContainer .btn-p {
+  position: absolute;
+  top: 0;
+  left: 0;
 }
 </style>
